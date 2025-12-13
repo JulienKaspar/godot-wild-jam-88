2f377468d5883d17134a4173188dc1bd83b4503f
bmo.green <bmogreenproject@gmail.com>
2025-12-13 23:55:48 +0100

    extra drunk music

--- Audio/music_manager.gd
+++ Audio/music_manager.gd
@@ -12,6 +12,8 @@ enum MUSIC_THEMES {
 	THEME_D,
 }
 
+var current_theme : MUSIC_THEMES = MUSIC_THEMES.THEME_A
+
 const MUSIC_BANK : Dictionary[MUSIC_THEMES, String] = {
 	MUSIC_THEMES.THEME_A : "theme_a",
 	MUSIC_THEMES.THEME_B : "theme_b",
@@ -22,7 +24,8 @@ const MUSIC_BANK : Dictionary[MUSIC_THEMES, String] = {
 enum DRUNKNESS_LAYERS {
 	DEFAULT = 0,
 	LOW = 1,
-	HIGH = 2
+	MED = 2,
+	HIGH = 3,
 }
 
 
@@ -42,9 +45,8 @@ func stop_music():
 
 func switch_music_to_theme(theme : MUSIC_THEMES):
 	music_player.set("parameters/switch_to_clip", MUSIC_BANK[theme])
-	update_drunkness_effect(drunkness_slider.value)
-
-
+	current_theme = theme
+	update_drunkness_effect(drunkness)
 
 ### === DRUNKNESS EFFECT === ##
 ## TODO: Move BUS information to global AudioManager class to expose bus volume settings
@@ -52,6 +54,11 @@ const BUS : Dictionary[String, int] = {
 	"MASTER" : 0,
 	"MUSIC" : 1,
 }
+
+var drunkness: float:
+	set(value):
+		drunkness = value
+		update_drunkness_effect(drunkness)
 
 ## TODO: save bus layout structure to ensure consistent effect indexing
 var stereo_enhancer_effect : AudioEffectStereoEnhance
@@ -65,41 +72,48 @@ func _setup_effect_handlers():
 	phaser_effect = AudioServer.get_bus_effect(BUS["MUSIC"], 2)
 	delay_effect = AudioServer.get_bus_effect(BUS["MUSIC"], 3)
 
+# The most important cutoff values
+const DRUNKNESS_LOW : float = 0.1
+const DRUNKNESS_MED : float = 0.35
+const DRUNKNESS_HIGH : float = 0.75
+
 func update_drunkness_effect(amount : float = -1.0):
 	if amount < 0.0 or amount > 1.0:
 		push_warning("music_manager.gd: update_drunk_music(amount) - amount parameter must be between 0.0 and 1.0")
 		return
 	
-	if amount > 0.0:
-		_get_current_theme_stream().set_sync_stream_volume(DRUNKNESS_LAYERS.LOW, linear_to_db(remap(amount, 0.0, 0.5, 0.0, 1.0)))
-	if amount > 0.5:
-		_get_current_theme_stream().set_sync_stream_volume(DRUNKNESS_LAYERS.HIGH, linear_to_db(remap(amount, 0.5, 1.0, 0.0, 1.0)))
+	if amount >= DRUNKNESS_LOW:
+		_get_current_theme_stream().set_sync_stream_volume(
+			DRUNKNESS_LAYERS.LOW, 
+			linear_to_db(remap(amount, DRUNKNESS_LOW, 1.0, 0.0, 1.0)))
+		
+	if amount >= DRUNKNESS_MED:
+		_get_current_theme_stream().set_sync_stream_volume(
+			DRUNKNESS_LAYERS.MED, 
+			linear_to_db(remap(amount, DRUNKNESS_MED, 1.0, 0.0, 1.0)))
+			
+	if amount >= DRUNKNESS_HIGH:
+		_get_current_theme_stream().set_sync_stream_volume(
+			DRUNKNESS_LAYERS.HIGH, 
+			linear_to_db(remap(amount, DRUNKNESS_HIGH, 1.0, 0.0, 1.0)))
+			
+		delay_effect.dry = 1.0 - amount * 0.25
+		delay_effect.tap1_level_db = linear_to_db(remap(amount, DRUNKNESS_HIGH, 1.0, 0.0, 0.25))
+		delay_effect.tap2_level_db = linear_to_db(remap(amount, DRUNKNESS_HIGH, 1.0, 0.0, 0.25))
+		delay_effect.feedback_level_db = linear_to_db(remap(amount, DRUNKNESS_HIGH, 1.0, 0.0, 0.25))
+		
+		phaser_effect.feedback = remap(amount, DRUNKNESS_HIGH, 1.0, 0.1, 0.4)
+		phaser_effect.rate_hz = remap(amount, DRUNKNESS_HIGH, 1.0, 0.01, 5.0)
+		phaser_effect.depth = remap(amount, DRUNKNESS_HIGH, 1.0, 0.1, 0.3)
+			
+	stereo_enhancer_effect.pan_pullout = remap(amount, DRUNKNESS_LOW, 1.0, 1.0, 4.0)
 	
-	if !is_instance_valid(stereo_enhancer_effect):
-		push_warning("AudioBus.Music:StereoEnhance effect could not be located")
-	else:
-		stereo_enhancer_effect.pan_pullout = remap(amount, 0.0, 1.0, 1.0, 4.0)
-		
-	if !is_instance_valid(chorus_effect):
-		push_warning("AudioBus.Music:Chorus effect could not be located")
-	else:
-		chorus_effect.dry = 1.0 - amount
-		chorus_effect.wet = remap(amount, 0.0, 1.0, 0.0, 0.7)
-		
-	if !is_instance_valid(phaser_effect):
-		push_warning("AudioBus.Music:Phaser effect could not be located")
-	else:
-		phaser_effect.feedback = remap(amount, 0.5, 1.0, 0.1, 0.4)
-		phaser_effect.rate_hz = remap(amount, 0.5, 1.0, 0.01, 5.0)
-		phaser_effect.depth = remap(amount, 0.5, 1.0, 0.1, 0.3)
-		
-	if !is_instance_valid(delay_effect):
-		push_warning("AudioBus.Music:Delay effect could not be located")
-	else:
-		delay_effect.dry = 1.0 - amount * 0.3
-		delay_effect.tap1_level_db = linear_to_db(remap(amount, 0.0, 1.0, 0.0, 0.1))
-		delay_effect.tap2_level_db = linear_to_db(remap(amount, 0.0, 1.0, 0.0, 0.1))
-		delay_effect.feedback_level_db = linear_to_db(remap(amount, 0.0, 1.0, 0.0, 0.1))
+	chorus_effect.dry = 1.0 - amount * 0.5
+	chorus_effect.wet = remap(amount, DRUNKNESS_MED, 1.0, 0.0, 0.7)
+
+	
+	
+	
 	
 	drunkness_label.text = str("Drunkness: ", amount)
 
@@ -122,3 +136,6 @@ func _connect_signals():
 func _connect_debug_ui():
 	theme_options.item_selected.connect(switch_music_to_theme)
 	drunkness_slider.value_changed.connect(update_drunkness_effect)
+
+func _update_drunkness(amount : float):
+	drunkness = amount
