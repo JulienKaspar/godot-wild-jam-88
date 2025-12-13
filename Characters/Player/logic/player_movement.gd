818d3a85690c81b8a8b649b0eea7dc10eff6e8e5
voki-toki <vokanimations@gmail.com>
2025-12-13 17:44:52 +0100

    player movement updates

--- Characters/Player/logic/player_movement.gd
+++ Characters/Player/logic/player_movement.gd
@@ -6,22 +6,35 @@ static var move_force_multiplier = 100.0
 static var player_input_strength = 1.0
 static var drunk_input_strength = 1.0
 static var drunk_chaos_speed = 1.0
-static var drunk_chaos_strength = 0.1
-static var drunk_fall_factor = 1.01 #how fast the falling will escalate
+static var drunk_chaos_strength = 0.2
+static var drunk_fall_factor = 4 #how fast the falling will escalate
 
 #----------------State-----------------------
 
 var drunk_noise_vector = Vector2(0,0)
 var drunk_amount = 0
 var player_move_dir = Vector2(0,0)
+var player_facing_dir = Vector2(0,1.0)
 
 #----------------Utility-------
 
 # generate some noise direction but tend to fall in one direction
 func update_drunk_vector(delta) -> void:
-	drunk_noise_vector *= delta * drunk_fall_factor
-	drunk_noise_vector.x += randf() * drunk_chaos_strength
-	drunk_noise_vector.x += randf() * drunk_chaos_strength
+	var new_noise = Vector2(randf() - 0.5,randf() - 0.5)
+	#new_noise.x = Noise.get_noise_1d(Time.get_ticks_msec())
+	drunk_noise_vector += player_move_dir * delta * drunk_fall_factor
+	#drunk_noise_vector += new_noise * drunk_chaos_strength
+	drunk_noise_vector = lerp(drunk_noise_vector, new_noise, drunk_chaos_strength)
+	
+
+func update_body_pose(delta) -> void:
+	$up_aligned.global_rotation = Vector3(0,0,0)
+	$upper_body_pivot.global_rotation = Vector3(0,0,0)
+
+func update_vectors() -> void:
+	player_move_dir.x = self.linear_velocity.x
+	player_move_dir.y = self.linear_velocity.z
+	
 
 #----------------Process-------
 
@@ -31,14 +44,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$upper_body_pivot.global_rotation = Vector3(0,0,0)
-	
+	update_body_pose(delta)
+	$up_aligned/helper_leaning.position = Vector3(player_move_dir.x,-0.22,player_move_dir.y)
 
 func _physics_process(delta: float) -> void:
+	update_vectors()
 	update_drunk_vector(delta)
 	# `velocity` will be a Vector2 between `Vector2(-1.0, -1.0)` and `Vector2(1.0, 1.0)`
 	var playerInputDir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-
+	$up_aligned/helper_player_dir.position = Vector3(playerInputDir.x,-0.21,playerInputDir.y)
+	$up_aligned/helper_drunk_dir.position = Vector3(drunk_noise_vector.x,-0.21,drunk_noise_vector.y)
+	
 	var move_force = playerInputDir * player_input_strength
 	move_force += drunk_noise_vector * drunk_input_strength
 	move_force *= delta * move_force_multiplier
