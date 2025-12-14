a4fb04d034332b3ada92f6a8789ccd4150176697
Roy ten Brink <brinkrrr@gmail.com>
2025-12-14 19:24:35 +0100

    debug player stats

--- Debug/debug_master_menu.gd
+++ Debug/debug_master_menu.gd
@@ -1,7 +1,8 @@
 extends Node
 class_name DebugMasterMenu
 
-@onready var player: Node3D = %Player
+@onready var player: Player = %Player
+
 @onready var player_drunkness: PlayerDrunkness = %Player/%Drunkness
 
 var displayed: bool = false
@@ -10,8 +11,13 @@ var displayed: bool = false
 func _process(_delta: float) -> void:
 	if displayed:
 		ImGui.Begin("Debug Menu")
+		ImGui.Text("Player Stats")
 		ImGui.Text("Drunkness: " + str(player_drunkness.current_drunkness))
-		
+		ImGui.Text("Player State" + str(player.inMoveState))
+		ImGui.Text("Player L Hand state " + str(player.HandLState))
+		ImGui.Text("Player R Hand State " + str(player.HandRState))
+		ImGui.Text("Player Facing Vector" + str(player.player_facing_dir))
+		ImGui.Text("Player Move Direction" + str(player.player_move_dir))
 		ImGui.End()
 
 func _unhandled_input(event: InputEvent) -> void:
