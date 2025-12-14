76424ab8f53986009f217076fcbdd0fe248b6ce4
Falk David <falkdavid@gmx.de>
2025-12-14 17:20:24 +0100

    Attach armature to physics attachment point

--- Characters/Player/logic/player.gd
+++ Characters/Player/logic/player.gd
@@ -4,12 +4,12 @@ class_name Player
 @onready var player_body: Node3D = $PlayerBody
 @onready var step_target: Node3D = $PlayerBody/StepTarget
 @onready var player_rb: Node3D = $PlayerController/RigidBally3D
-@onready var upper_body_pivot: Node3D = $PlayerController/RigidBally3D/upper_body_pivot
+@onready var body_attach_point: Node3D = $PlayerController/UpperBody/BodyAttachPoint
 @onready var pickup_radius: ShapeCast3D = $PickupRadius
 
 
-func _physics_process(_delta: float) -> void:
-	player_body.global_transform = lerp(player_body.global_transform, upper_body_pivot.global_transform, .5)
+func _process(_delta: float) -> void:
+	player_body.global_transform = lerp(player_body.global_transform, body_attach_point.global_transform, .5)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pickup"):
