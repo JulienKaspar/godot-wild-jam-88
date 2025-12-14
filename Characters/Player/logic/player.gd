d6f55a81004681c953da3a0ac9ffb95025b39835
voki-toki <vokanimations@gmail.com>
2025-12-14 17:33:07 +0100

    moved some states to player script

--- Characters/Player/logic/player.gd
+++ Characters/Player/logic/player.gd
@@ -7,9 +7,52 @@ class_name Player
 @onready var body_attach_point: Node3D = $PlayerController/UpperBody/BodyAttachPoint
 @onready var pickup_radius: ShapeCast3D = $PickupRadius
 
+### statess 
+enum MoveStates {IDLE, MOVING, FALLING, ROLLING, FLASKY, FELL} 
+enum HandStates {DANGLY, REACHING, HOLD, DRINKING}
+
+# input states
+var grabbingL = false
+var grabbingR = false
+
+# move state
+var inMoveState = MoveStates.IDLE
+var HandLState = HandStates.DANGLY
+var HandRState = HandStates.DANGLY
+var canRoll = true
+var player_speed = 0.0 # owned by player_controller
+var player_move_dir = Vector2(0,0) # owned by player_controller
+var player_facing_dir = Vector2(0,1.0) # owned by player_controller
+var leaning = 0.0 # owned by player_controller
+
+# health and safety
+var drunk_amount = 0.5
+
+#----------------------------------------
+
+func goRoll() -> void:
+	pass
 
 func _process(_delta: float) -> void:
 	player_body.global_transform = lerp(player_body.global_transform, body_attach_point.global_transform, .5)
+	
+	# ------- Input Handling ------
+	if Input.is_action_pressed("grab_left"):
+		grabbingL = true
+	else:
+		grabbingL = false
+		
+	if Input.is_action_pressed("grab_right"):
+		grabbingR = true
+	else:
+		grabbingR = false
+		
+	if Input.is_action_just_pressed("roll"):
+		if canRoll:
+			goRoll()
+		else:
+			# WIP, player feedback fo unsuccessfull roll?
+			pass
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pickup"):
