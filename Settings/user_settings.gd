0119e16a50a05445bf04a61a1fe46d2d33a6961b
rBrink21 <brinkrrr@gmail.com>
2025-12-15 18:42:05 +0100

    user settings menu, autoload and audiosliders

---
+++ Settings/user_settings.gd
@@ -0,0 +1,10 @@
+#user_settings.gd (autoload)
+extends Node
+
+var sensitivity: float = 1
+var drunk_visual_effect_intensity: float = 1
+var master_volume: float = 1
+var ui_volume: float = 1
+var music_volume: float = 1
+var sfx_volume: float = 1
+var disorienting_sounds_enabled: bool = true
--- UI/SettingsMenu/settings_menu.gd
+++ UI/SettingsMenu/settings_menu.gd
@@ -1,23 +1,12 @@
 extends PanelContainer
 class_name SettingsMenu
 
-@onready var sensitivity_slider: HSlider = %SensitivitySlider
-@onready var sensitivity_number: Label = %SensitivityNumber
 @onready var main_menu_button: Button = %MainMenuButton
 
 signal main_menu_button_pressed()
 
 func _ready() -> void:
-	
-	sensitivity_slider.value_changed.connect(handle_sensitivity_slider_changed)
 	main_menu_button.pressed.connect(handle_main_menu_button_pressed)
-	
-	sensitivity_number.text = str(ProjectSettings.get_setting("sensitivity", 1))
-	sensitivity_slider.value = ProjectSettings.get_setting("sensitivity", 1)
-
-func handle_sensitivity_slider_changed(value: float) -> void:
-	ProjectSettings.set_setting("sensitivity", sensitivity_slider.value)
-	sensitivity_number.text = str(value)
 
 func handle_main_menu_button_pressed() -> void:
 	main_menu_button_pressed.emit()
