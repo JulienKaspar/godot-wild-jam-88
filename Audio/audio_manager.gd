71638131e1255da645619de6dfb5abb10cf0f549
bmo.green <bmogreenproject@gmail.com>
2025-12-16 13:52:37 +0100

    set up title screen ambience and ui sound manager

--- Audio/audio_manager.gd
+++ Audio/audio_manager.gd
@@ -23,3 +23,31 @@ enum FX {
 }
 
 var music_manager : MusicManager
+var ui_sounds : UI_Sounds = UI_Sounds.new()
+
+# Fading audio
+func fade_audio(node: Node, _target_volume_linear : float = -1.0, _fade_speed : float = 1.5) -> void:
+	# Check for correct use
+	if !_is_audio_player(node):
+		push_warning(str("AudioManager: fade_in_audio() cannot process node: ", node, " has to be of type AudioStreamPlayer(/2D/3D)"))
+		return
+	elif _target_volume_linear < 0.0:
+		push_warning("AudioManager: fade_in_audio() requires _target_volume_linear to be set (0.0 for fade out, > 0.0 for fade in)")
+		return
+	
+	# Ensure node's volume is off if target is on
+	if _target_volume_linear > 0.0:
+		node.set("volume_db", -60.0)
+	
+	# Tween audio to target volume
+	var tween : Tween = create_tween()
+	tween.tween_property(node, "volume_db", linear_to_db(_target_volume_linear), _fade_speed)
+
+func _is_audio_player(node : Node) -> bool:
+	var is_audio_player : bool = (
+		node is AudioStreamPlayer
+		or node is AudioStreamPlayer2D
+		or node is AudioStreamPlayer3D
+	)
+	
+	return is_audio_player
