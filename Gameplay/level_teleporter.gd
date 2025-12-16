708f1514e1948c541426c014d04ecb1f20b62cbc
Roy ten Brink <brinkrrr@gmail.com>
2025-12-16 21:21:17 +0100

    teleporter

--- Gameplay/level_teleporter.gd
+++ Gameplay/level_teleporter.gd
@@ -4,8 +4,8 @@ class_name LevelTeleporter
 @export var teleport_position: Node3D
 
 func _ready() -> void:
-	body_entered.connect(handle_body_entered)
-
-func handle_body_entered(body: Node3D) -> void:
-	if body is PlayerController:
-		GameStateManager.move_player_and_reset.emit(teleport_position)
+	area_entered.connect(teleport)
+	
+func teleport(area: Area3D) -> void:
+	if area is PlayerDetector:
+		print("DETECTED")
