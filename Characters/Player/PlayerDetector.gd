04677f5871f4e74fbfcd5f5c0e663a4e4ecf9e71
Roy ten Brink <brinkrrr@gmail.com>
2025-12-16 22:15:37 +0100

    player detector

--- Characters/Player/PlayerDetector.gd
+++ Characters/Player/PlayerDetector.gd
@@ -1,2 +1,11 @@
 extends Area3D
 class_name PlayerDetector
+
+@export var detetection_radius: float = 2
+
+func _process(_delta: float) -> void:
+	var player_position: Vector3 = GameStateManager.current_player.player_global_pos
+	var distance: float = (player_position - position).length()
+	
+	if distance < detetection_radius:
+		print("YELL")
