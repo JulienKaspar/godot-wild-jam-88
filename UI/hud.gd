3707f83103c7e37d0e0aba8238add61683fb4cfb
Roy ten Brink <brinkrrr@gmail.com>
2025-12-16 17:17:26 +0100

    hud 3

--- UI/hud.gd
+++ UI/hud.gd
@@ -4,6 +4,7 @@ class_name HUD
 @onready var drunkness_meter: TextureProgressBar = %DrunknessMeter
 @onready var player_drunkness: PlayerDrunkness = %Player/%Drunkness
 @onready var drunkness_marker: TextureRect = %DrunknessMarker
+@onready var marker_margin: float = 50
 
 func _ready() -> void:
 	drunkness_meter.min_value = player_drunkness.min_drunkness
@@ -13,4 +14,14 @@ func _ready() -> void:
 	
 func update_meter(new_value: float) -> void:
 	drunkness_meter.value = new_value
+	
+func _process(_delta: float) -> void:
+	update_drunkness_marker_position()
+
+func update_drunkness_marker_position() -> void:
+	var normalized_progress = drunkness_meter.value /drunkness_meter.max_value 
+	var bar_rect = drunkness_meter.get_rect()
+	var new_x = bar_rect.position.x + (bar_rect.size.x * normalized_progress)
+
+	drunkness_marker.position = Vector2(clampf(new_x,marker_margin, bar_rect.size.x - marker_margin),drunkness_marker.position.y)
 	
