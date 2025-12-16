9c0648e7cd3d2a37f4527f46f5dfec897b5486b8
Roy ten Brink <brinkrrr@gmail.com>
2025-12-16 14:33:44 +0100

    finishing touches

--- UI/dialogue_system.gd
+++ UI/dialogue_system.gd
@@ -8,7 +8,7 @@ class_name DialogueSystem
 @onready var dialogue_starting_position: Vector2 = dialogue_prompt.position
 
 @export var display_time_seconds: float = 13
-@export var text_bubble_up_delay: float = 3
+@export var text_bubble_up_delay: float = 1.5
 @export var text_bubble_up_time_seconds_initial: float = 1
 @export var text_bubble_up_time_seconds_middle: float = 2
 @export var text_bubble_up_time_seconds_late: float = 0.5
@@ -40,21 +40,21 @@ func display_dialogue(text: String) -> void:
 	var gradient_transparency_tween : Tween = create_tween()
 	gradient_transparency_tween = create_tween()
 	gradient_transparency_tween.tween_property(gradient,"self_modulate", gradient_end_transparency, dialogue_float_up_duration)
-	
-	var visible_ratio_tween: Tween = create_tween()
-	visible_ratio_tween.tween_property(dialogue_text, "visible_ratio", 0.25, text_bubble_up_time_seconds_initial).set_ease(Tween.EASE_IN_OUT)
-	visible_ratio_tween.tween_property(dialogue_text, "visible_ratio", 0.5, text_bubble_up_time_seconds_middle).set_ease(Tween.EASE_IN_OUT)
-	visible_ratio_tween.tween_property(dialogue_text, "visible_ratio", 0.75, text_bubble_up_time_seconds_late).set_ease(Tween.EASE_IN_OUT)
-	visible_ratio_tween.tween_property(dialogue_text, "visible_ratio", 1, text_bubble_up_time_seconds_final).set_ease(Tween.EASE_IN_OUT)
 
 	wobble_flasky()
 	
 	var prompt_position_tween : Tween = create_tween()
 	prompt_position_tween.tween_property(dialogue_prompt, "position", dialogue_ending_position, dialogue_float_up_duration).set_ease(Tween.EASE_IN_OUT)
 	
+	await get_tree().create_timer(text_bubble_up_delay).timeout
+	start_showing_text()
+
+	
+	
 func _process(delta: float) -> void:
 	if dialogue_prompt.visible:
 		displayed_time += delta
+		
 		if displayed_time >= display_time_seconds:
 			dialogue_prompt.hide()
 		if displayed_time + fade_out_duration >= display_time_seconds:
@@ -77,3 +77,10 @@ func fade_out_flasky() -> void:
 	
 	var gradient_tween: Tween = create_tween()
 	gradient_tween.tween_property(gradient, "self_modulate", gradient_start_transparency, fade_out_duration).set_ease(Tween.EASE_IN_OUT)
+
+func start_showing_text() -> void:
+	var visible_ratio_tween: Tween = create_tween()
+	visible_ratio_tween.tween_property(dialogue_text, "visible_ratio", 0.25, text_bubble_up_time_seconds_initial).set_ease(Tween.EASE_IN_OUT)
+	visible_ratio_tween.tween_property(dialogue_text, "visible_ratio", 0.5, text_bubble_up_time_seconds_middle).set_ease(Tween.EASE_IN_OUT)
+	visible_ratio_tween.tween_property(dialogue_text, "visible_ratio", 0.75, text_bubble_up_time_seconds_late).set_ease(Tween.EASE_IN_OUT)
+	visible_ratio_tween.tween_property(dialogue_text, "visible_ratio", 1, text_bubble_up_time_seconds_final).set_ease(Tween.EASE_IN_OUT)
