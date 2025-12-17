468f2febda3e4f4953563055354656472e22ae14
Roy ten Brink <brinkrrr@gmail.com>
2025-12-17 22:36:56 +0100

    movement utils

--- Gameplay/player_movement_utils.gd
+++ Gameplay/player_movement_utils.gd
@@ -39,4 +39,4 @@ static func force_body_towards(position: Vector3, force_multiplier: float) -> vo
 static func slip_player(player_position: Vector3, force_multiplier: float) -> void:
 	var ball_momentum_direction: Vector3 = GameStateManager.current_player.get_ball().linear_velocity.normalized()
 	force_ball_towards(player_position + ball_momentum_direction, force_multiplier)
-	force_body_towards(player_position - ball_momentum_direction, force_multiplier / 5)
+	force_body_towards(player_position - ball_momentum_direction, force_multiplier / 3)
