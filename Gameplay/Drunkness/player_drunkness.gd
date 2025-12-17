d3bfdad318765e9315598e0be4a2f4020fbfe473
Roy ten Brink <brinkrrr@gmail.com>
2025-12-17 20:57:45 +0100

    player drunkness audio spot

--- Gameplay/Drunkness/player_drunkness.gd
+++ Gameplay/Drunkness/player_drunkness.gd
@@ -5,6 +5,7 @@ var min_drunkness: float = 0
 var max_drunkness: float = 10
 var starting_drunkness: float = 5
 var drunkness_decay_per_second: float = 0.1
+var threshold: float = 0.1
 
 signal on_drunkness_changed(new_value: float)
 
@@ -14,6 +15,11 @@ signal on_too_drunk()
 
 var current_drunkness: float: 
 	set(value):
+		var drunkness_delta = current_drunkness - value
+		if drunkness_delta >= threshold:
+			@warning_ignore("standalone_ternary")
+			drunkness_increased() if current_drunkness < value else drunkness_decreased()
+		
 		current_drunkness = value
 		set_drunkness(value)
 
@@ -27,6 +33,12 @@ func set_drunkness(_new_value: float) -> void:
 		on_too_drunk.emit()
 		current_drunkness = max_drunkness
 		return
+	
+func drunkness_increased() -> void:
+	pass
+	
+func drunkness_decreased() -> void:
+	pass
 	
 func _init() -> void:
 	reset_drunkness()
