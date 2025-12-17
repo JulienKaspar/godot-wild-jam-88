867ec0f58c02b4168b75c9d19d6b8e9b70b15493
Roy ten Brink <brinkrrr@gmail.com>
2025-12-17 14:04:52 +0100

    fixed max mumbling

--- Audio/SFX/radio_player.gd
+++ Audio/SFX/radio_player.gd
@@ -1,35 +1,33 @@
 extends AudioStreamPlayer3D
 
-@export var voice_trigger_area : Area3D
+@export var voice_trigger_radius: float = 5
 
 var loop_clip : AudioStreamSynchronized
 var body_target : Node3D
 var voice_active : bool = false
 
 func _ready():
-	voice_trigger_area.body_entered.connect(_on_player_body_entered)
-	voice_trigger_area.body_exited.connect(_on_player_body_exited)
+
 	var _stream : AudioStreamInteractive = self.stream as AudioStreamInteractive
 	loop_clip = _stream.get_clip_stream(1) # song loop
-	
-
-func _on_player_body_entered(body):
-	if body.name == "UpperBody":
-		print("UPPER BODY DETECTED")
 		
-		voice_active = true
-		body_target = body
-		
-
-func _on_player_body_exited(body):
-	if body.name == "UpperBody":
-		print("UPPER BODY LEFT")
-		
-		loop_clip.set_sync_stream_volume(1, -60.0)
-		voice_active = false
+func get_player_distance() -> float:
+	var player_pos = GameStateManager.current_player.player_global_pos
+	var distance = get_parent_node_3d().get_parent_node_3d().position.distance_to(player_pos)
+	return distance
 
 func _process(_delta):
+	print(get_player_distance())
+	
+	if get_player_distance() < voice_trigger_radius:
+		voice_active = true
+	else:
+		loop_clip.set_sync_stream_volume(1, -60.0)
+		voice_active = false
+	
 	if voice_active:
-		var distance : float = self.position.distance_squared_to(body_target.position)
-		var attenuation_volume : float = clampf(remap(distance, 50.0, 5.0, -60, -3.0), -60, -3.0)
+		var distance_squared : float = get_player_distance() * get_player_distance()
+		var attenuation_volume : float = clampf(remap(distance_squared, 50.0, 5.0, -60, -3.0), -60, -3.0)
 		loop_clip.set_sync_stream_volume(1, attenuation_volume)
+	
+		
