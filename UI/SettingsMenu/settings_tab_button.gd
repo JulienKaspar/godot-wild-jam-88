68b4366e2654b75d91012317a668c307dbc3d7cd
Roy ten Brink <brinkrrr@gmail.com>
2025-12-18 22:52:44 +0100

    oopsies

--- UI/SettingsMenu/settings_tab_button.gd
+++ UI/SettingsMenu/settings_tab_button.gd
@@ -3,6 +3,7 @@ class_name SettingsTabButton
 
 @export var focus_scale: float = 1.1
 @export var focus_speed: float = 0.2
+@export var focused_margin_value: int = 35
 
 func _ready() -> void:
 	focus_entered.connect(select)
@@ -12,9 +13,24 @@ func select() -> void:
 	var size_tween: Tween = create_tween()
 	size_tween.tween_property(self, 'scale', Vector2(focus_scale, focus_scale), focus_speed)
 	z_index = 10
-	
+	enable_side_margin()
 	
 func deselect() -> void:
 	var size_tween: Tween = create_tween()
 	size_tween.tween_property(self, 'scale', Vector2(1,1), focus_speed)
 	z_index = 0
+	disable_side_margin()
+
+func disable_side_margin() -> void:
+	var parent: Node = get_parent()
+	if parent is MarginContainer:
+		var margin_container: MarginContainer = parent as MarginContainer
+		margin_container.remove_theme_constant_override("margin_left")
+		margin_container.remove_theme_constant_override("margin_right")
+
+func enable_side_margin() -> void:
+	var parent: Node = get_parent()
+	if parent is MarginContainer:
+		var margin_container: MarginContainer = parent as MarginContainer
+		margin_container.add_theme_constant_override("margin_left", focused_margin_value)
+		margin_container.add_theme_constant_override("margin_right", focused_margin_value)
