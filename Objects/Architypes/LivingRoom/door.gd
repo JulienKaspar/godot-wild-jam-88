0975b95b7afd15659aeaa34a746a504557aead77
JulienKaspar <julien.kaspar@googlemail.com>
2025-12-18 19:11:50 +0100

    Swappable material on door asset

--- Objects/Architypes/LivingRoom/door.gd
+++ Objects/Architypes/LivingRoom/door.gd
@@ -1,10 +1,25 @@
+@tool
 extends Node3D
 
-@onready var lamp : SpotLight3D = $SpotLight3D
+@export var wall_material : Material
 
+@onready var lamp : SpotLight3D = $SpotLight3D
 @onready var original_brighness := lamp.light_energy
 
+
+func _ready() -> void:
+	for node in get_children():
+		if node is CSGBox3D:
+			node.set_material(wall_material)
+
+
 func _process(_delta: float) -> void:
+	
+	if Engine.is_editor_hint():
+		for node in get_children():
+			if node is CSGBox3D:
+				var csg_box : CSGBox3D = node
+				csg_box.set_material_override(wall_material)
 	
 	var strength := 2.0
 	var sine_factor := sin(Time.get_ticks_msec() * 0.001 * strength)
