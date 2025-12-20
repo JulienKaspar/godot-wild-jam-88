a98f52ee358ff2a768962da9017480a9da7a7a56
JulienKaspar <julien.kaspar@googlemail.com>
2025-12-20 11:21:32 +0100

    Reset spawn point and placed more beers
    RIght before the indi keg there are some cans on the stairs above to grab.
    Also adjusted the stairs layers to allow props and beer to collide with the steps instead of the slope.

--- Objects/slopes.gd
+++ Objects/slopes.gd
@@ -1,8 +1,12 @@
 extends StaticBody3D
 class_name Slope
 
-var collision_layers := [1, 8]
+var collision_layers := [8]
 
 func _ready() -> void:
+	for idx in range(1, 32):
+		set_collision_layer_value(idx, false)
+		set_collision_mask_value(idx, false)
+	
 	for idx in collision_layers:
 		set_collision_layer_value(idx, true)
