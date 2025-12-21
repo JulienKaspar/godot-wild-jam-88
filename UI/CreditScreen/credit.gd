cc1f00724b9beb60c7054a51a14bc0d0a1eb3ebf
Roy ten Brink <brinkrrr@gmail.com>
2025-12-21 05:26:51 +0100

    credit menu

---
+++ UI/CreditScreen/credit.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name Credit
+
+@export_enum("Roy ten Brink", "Viktor Ikkes", "Julien Kaspar", "Clément Foucault", "Weizhen Huang", "Vivien Lulkowski", "Simon Thommes", "Falk David", "Max de Kroon") var author: String
+@export var commit: String
---
+++ UI/CreditScreen/credit_screen.gd
@@ -0,0 +1,27 @@
+extends PanelContainer
+class_name CreditScreen
+
+@export var scroll_speed: float = 100
+@export var start_delay: float = 1
+@export var commits: Array[Credit]
+@export var credit_scene: PackedScene
+@onready var grid_container: GridContainer = %GridContainer
+@onready var scroll_container: ScrollContainer = %ScrollContainer
+var time_elapsed: float = 0 
+var scrolled: float
+
+
+func _ready() -> void:
+
+	for commit in commits:
+		var instance: CreditItem = credit_scene.instantiate()
+		grid_container.add_child(instance)
+		instance.display(commit)
+	
+	
+
+func _process(delta: float) -> void:
+	time_elapsed += delta
+	if time_elapsed > start_delay:
+		scrolled += delta * scroll_speed
+		scroll_container.scroll_vertical = roundi(scrolled)
