5bbf87bfa6103d053966b26a5b1a9ca57d5eb58f
rBrink21 <brinkrrr@gmail.com>
2026-01-18 11:56:05 +0100

    buttons are not keys and keys are not buttons, idiot

--- UI/Screens/WastedScreen/wasted_screen.gd
+++ UI/Screens/WastedScreen/wasted_screen.gd
@@ -3,7 +3,7 @@ class_name WastedScreen
 
 signal on_continued()
 
-func _unhandled_key_input(event: InputEvent) -> void:
+func _unhandled_input(event: InputEvent) -> void:
 	if !visible:  return
 	if event.is_action_pressed("roll"):
 		GameStateManager.unpause_game()
