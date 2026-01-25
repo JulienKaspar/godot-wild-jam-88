0bb8f354683000d2c7f00e233750ed2581ff93b0
rBrink21 <brinkrrr@gmail.com>
2026-01-25 14:34:18 +0100

    deleting keg_monitor safely for now as to not interfere with .tscn changes

--- Gameplay/Achievements/Triggers/keg_monitor.gd
+++ Gameplay/Achievements/Triggers/keg_monitor.gd
@@ -2,13 +2,3 @@ extends Node
 
 @export var keg: RigidBody3D
 var touched_keg: bool = false
-
-func _enter_tree() -> void:
-	listen_to_keg_trigger.call_deferred()
-
-func listen_to_keg_trigger() -> void:
-	keg.body_entered.connect(handle_keg_collision)
-	
-func handle_keg_collision(body: Node3D) -> void:
-	if PlayerDetector.has_player_as_parent(body):
-		AchievementSystem.unlock_achievement(Achievement.ID.DodgeKeg)
