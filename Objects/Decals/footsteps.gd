47bd54e6cad74334a95f304efc40cfef218efff0
JulienKaspar <julien.kaspar@googlemail.com>
2026-02-01 14:22:17 +0100

    Updated footsteps decals

--- Objects/Decals/footsteps.gd
+++ Objects/Decals/footsteps.gd
@@ -4,13 +4,12 @@ extends Node3D
 @export var modulate_color : Color = Color.WHITE
 @export var textures: Array[CompressedTexture2D] = []
 
-@onready var mesh : MeshInstance3D = $MeshInstance3D
-@onready var material : StandardMaterial3D = mesh.get_active_material(0)
+@onready var decal : Decal = $Decal
 
 func _ready() -> void:
-	material.albedo_texture = textures.pick_random()
+	decal.texture_albedo = textures.pick_random()
 
 
 func _process(delta: float) -> void:
-	material.albedo_color = modulate_color
+	decal.modulate = modulate_color
 	
