7457ab770cf08aa04a3be8cf273003b8111c0765
bmo.green <bmogreenproject@gmail.com>
2026-03-10 17:38:40 +0100

    add prop collisions and volume changes

---
+++ Objects/prop_collider.gd
@@ -0,0 +1,20 @@
+extends RigidBody3D
+class_name PropCollider
+
+func _ready():
+	body_entered.connect(on_collision)
+
+func on_collision(_body) -> void:
+	if self.linear_velocity.length() > 0.5:
+		var sound : AudioStreamPlayer3D = AudioManager.sfx_manager.get_item()
+		sound.position = self.global_position
+		sound.reparent(self)
+		sound.stream = AudioManager.sfx_manager.prop_collision_sounds
+		sound.volume_db = remap(
+				self.linear_velocity.length(), 
+				0.5, 2.0, # velocity input range
+				-3.0, 1.5 # volume output range
+			)
+		sound.volume_db = clampf(sound.volume_db, -3.0, 1.5)
+		sound.play()
+		print("Prop: ", get_parent().name, " colliding with: ", _body.name, " at velocity: ", self.linear_velocity.length())
